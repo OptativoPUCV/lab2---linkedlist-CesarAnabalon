769cb2a96e86682038c510a3d6090a9b0200a0a2
Replit user <>
2023-03-21 23:44:08 +0000

    new try

--- list.c
+++ list.c
@@ -44,7 +44,7 @@ if(!list->head) return NULL;
 }
 
 void * nextList(List * list) {
-  if(!list->head->next) return NULL;
+  if(!list->head) return NULL;
   if(!list->current) return NULL;
   
   list->head =list->head->next; 
