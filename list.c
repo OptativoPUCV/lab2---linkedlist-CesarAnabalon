3a57df7305504f7fe726948287eeeaec099a1682
Replit user <>
2023-03-22 01:11:59 +0000

    new try

--- list.c
+++ list.c
@@ -50,7 +50,7 @@ void * nextList(List * list) {
     
   list->current=list->head;
     
-    return (list->head->data)&&(list->current);
+    return (list->head);
 }
 
 void * lastList(List * list) {
